c21449b5ef87c29ded3575b0b2d8ae08bb351e18
CorbynL <corbynlangley@gmail.com>
2018-03-22 16:22:40 +0200

    Cow class

---
+++ MorabarabaV2/Cow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorabarabaV2
+{ 
+    public class Cow
+    {
+        public int Position { get; set; }
+        public char UserId { get; set; }
+        public int CowNumber { get; set; }
+        public int Id { get; set; }
+
+        public Cow(int Position = -1, char UserId = ' ', int CowNumber = -1, int Id = -1)
+        {
+            this.Position = Position;
+            this.UserId = UserId;
+            this.CowNumber = CowNumber;
+            this.Id = Id;
+
+        }
+
+
+        //Do things
+
+
+    }
+}
