42384d501700fcbc9d0771ee693d3cdc271b1f70
CorbynL <corbynlangley@gmail.com>
2018-03-22 16:24:40 +0200

    Board class

---
+++ MorabarabaV2/Board.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorabarabaV2
+{
+    public class Board
+    {
+        public Cow[] Cows;
+
+
+        public Board(Cow[] cows)
+        {
+            Cows = cows;
+
+        }
+
+
+    }
+}
