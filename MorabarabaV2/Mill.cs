7ae3e931277377e7dbb0b2dcb9aa79a53cf1e9c0
CorbynL <corbynlangley@gmail.com>
2018-03-22 16:26:23 +0200

    mill class

---
+++ MorabarabaV2/Mill.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorabarabaV2
+{
+    public class Mill
+    {
+        Cow[] Cows;
+
+        public Mill(Cow[] cows)
+        {
+            Cows = cows;
+        }
+    }
+}
