fd531460bace01144fd528c4e6e4099a97120fb1
CorbynL <corbynlangley@gmail.com>
2018-03-24 12:33:47 +0200

    convert mill members for gui use

--- MorabarabaV2/BaseNotificationClass.cs
+++ MorabarabaV2/BaseNotificationClass.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace MorabarabaV2
 {
-    class BaseNotificationClass
+    public class BaseNotificationClass : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
