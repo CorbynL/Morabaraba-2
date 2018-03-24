cffb490531342c1ad9f48041802b6f62644858d7
CorbynL <corbynlangley@gmail.com>
2018-03-24 13:53:57 +0200

    game session class

---
+++ MorabarabaV2/GameSession.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorabarabaV2
+{
+    public class GameSession : BaseNotificationClass
+    {
+        private Board _board;
+       public Board board
+        {
+            get { return _board; }
+            set
+            {
+                _board = value;
+                OnPropertyChanged(nameof(board));
+            }
+        }
+
+        public GameSession()
+        {
+            board = new Board();
+            board.CreateEmptyMills();
+        }
+    }
+}
